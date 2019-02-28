c7929fdc1c9c086d02aa923bc7fa7c6e0a012756
Watinguyaz <watimakhanu@gmail.com>
2019-02-28 13:05:19 +0300

    take 35

--- tribune/settings.py
+++ tribune/settings.py
@@ -146,11 +146,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# configuring the location for media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Configure Django App for Heroku.
 django_heroku.settings(locals())
