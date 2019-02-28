5289148778cc899e3c0536825bec665d0d9900d4
Watinguyaz <watimakhanu@gmail.com>
2019-02-28 12:38:35 +0300

    all done

---
+++ news/tests.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from .models import Editor,Article,tags
+import datetime as dt
+
+# Create your tests here.
+class EditorTestCase(TestCase):
+    def setUp(self):
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+        self.james.save_editor()
+
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
+
+    def test_get_news_today(self):
+        news_of_day = Article.news_of_day()
+        self.assertTrue(len(news_of_day)>0)
+
+    def test_get_news_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)
